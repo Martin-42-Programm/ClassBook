8a63a8076583ce33e008b3af1a6adbacc2498525
Valx7xx <163676393+Valx7xx@users.noreply.github.com>
2024-03-20 21:33:32 +0200

    Add files via upload

---
+++ ClassBook/Repository/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassBook.Repository
+{
+	public class UserRepository : IGenericRepository<User>
+	{
+        private ApplicationContext _context;
+
+        public UserRepository(ApplicationContext dbContext)
+		{
+            _context = dbContext;
+        }
+
+        public bool Add(User item)
+        {
+            try
+            {
+                _context.Add<User>(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+           
+        }
+
+        public bool Delete(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
