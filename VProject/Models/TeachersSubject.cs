f9649e45cba9f66ae44ee59a2cbf7f142af094e2
Valx7xx <163676393+Valx7xx@users.noreply.github.com>
2024-03-21 16:59:53 +0200

    Add files via upload

---
+++ VProject/Models/TeachersSubject.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassPage.Models
+{
+    public class TeachersSubject
+    {
+        public int TeacherId { get; set; }
+        public int SubjectId { get; set; }
+
+        public virtual Subject Subject { get; set; }
+        public virtual Teacher Teacher { get; set; }
+    }
+}
