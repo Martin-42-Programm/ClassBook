571426e50257eac92cb697d8df326256f67ff5c7
Valx7xx <163676393+Valx7xx@users.noreply.github.com>
2024-03-22 20:04:17 +0200

    Add files via upload

---
+++ VProject/Controllers/GradeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolDiary.Controllers
+{
+  
+    public class GradeController : Controller
+    {
+        private IGradesService gradesService;
+
+        public GradeController(IGradesService gradesService)
+        {
+            this.gradesService = gradesService;
+        }
+
+        public IHttpActionResult GetGrade(int id)
+        {
+            if(gradesService.ExistsID(id) == false)
+            {
+                return NotFound();
+            }
+            return Ok(gradesService.GetByID(id));
+        }
+
+     
+        public IHttpActionResult PutGrade(int id, Grade grade)
+        {
+            if (gradesService.ExistsID(id) == false)
+            {
+                return NotFound();
+            }
+
+            return Ok(gradesService.PutGrade(id, grade));
+        }
+
+       
+        public IHttpActionResult DeleteGrade(int id)
+        {
+            if(gradesService.ExistsID(id) == false)
+            {
+                return NotFound();
+            }
+            return Ok(gradesService.DeleteByID(id));
+        }
+    }
+}
