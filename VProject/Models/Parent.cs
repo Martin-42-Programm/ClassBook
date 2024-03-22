53c85e3026b067fae20ef7935bc690980b686e50
Valx7xx <163676393+Valx7xx@users.noreply.github.com>
2024-03-22 20:25:11 +0200

    Add files via upload

---
+++ VProject/Models/Parent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicSchoolDiary.Models
+{
+    class Parent
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone_number { get; set; }
+
+
+        public Parent( string name, string surname, string address,string email, string phone_number)
+        {
+            if (phone_number != "" || phone_number.Length == 9 )
+            {
+                Name = name;
+                Surname = surname;
+                Address = address;
+                Email = email;
+                Phone_number = phone_number;
+            }
+            else
+                throw new Exception("enter a nine digit phone number");
+        }
+    }
+}
