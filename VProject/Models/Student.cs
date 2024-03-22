f723d477a92f5935ddc49da42eb10a2822cbfbd2
Valx7xx <163676393+Valx7xx@users.noreply.github.com>
2024-03-22 20:21:33 +0200

    Add files via upload

---
+++ VProject/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ElectronicSchoolDiary.Models
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Phone_number { get; set; }
+
+
+        public Student(string name, string surname, string address, string phone_number)
+        {
+            {
+                if (phone_number.Length != 0 || phone_number.Length == 9)
+                {
+                    Name = name;
+                    Surname = surname;
+                    Address = address;
+                    Phone_number = phone_number;
+                }
+                else if(phone_number.Length > 0 && phone_number.Length != 9) throw new Exception("enter a nine digit phone number.");
+            }
+            
+        }
+    }
+}
